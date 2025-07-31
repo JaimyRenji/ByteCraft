4a0a0e7e7cae5a6510960579af3365819be1c6db
Jeevesh Binoy <151763733+JeeveshBinoy@users.noreply.github.com>
2025-07-31 18:30:26 +0530

    Update budgetRoutes.js

--- expense-planner/server/routes/budgetRoutes.js
+++ expense-planner/server/routes/budgetRoutes.js
@@ -1,16 +1,1 @@
-import express from 'express';
-import {
-  setBudget,
-  getBudgets,
-  getBudgetSummary
-} from '../controllers/budgetController.js';
 
-const router = express.Router();
-
-router.post('/set', setBudget);
-
-router.get('/', getBudgets);
-
-router.get('/summary', getBudgetSummary);
-
-export default router;
